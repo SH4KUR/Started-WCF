69a72b1eb9b590a789fbccea870b8fa53ae586d0
Dmitriy Lopatiev <dmitriy.lopatiev@gmail.com>
2020-07-23 15:12:14 +0300

    Code review

--- GettingStartedHost/Program.cs
+++ GettingStartedHost/Program.cs
@@ -13,19 +13,26 @@ namespace GettingStartedHost
     {
         static void Main(string[] args)
         {
+            // Create a URI to serve as the base address.
             var baseAddress = new Uri("http://localhost:8000/GettingStarted/");
+
+            // Create a ServiceHost instance.
             var selfHost = new ServiceHost(typeof(CalculatorService), baseAddress);
 
             try
             {
+                // Add a service endpoint.
                 selfHost.AddServiceEndpoint(typeof(ICalculator), new WSHttpBinding(), "CalculatorService");
 
+                // Enable metadata exchange.
                 var smb = new ServiceMetadataBehavior { HttpGetEnabled = true };
                 selfHost.Description.Behaviors.Add(smb);
 
+                // Start the service.
                 selfHost.Open();
                 Console.WriteLine("The service is ready");
 
+                // Close the ServiceHost to stop the service.
                 Console.WriteLine("Press <Enter> to terminate the service\n");
                 Console.ReadLine();
 
@@ -35,6 +42,7 @@ namespace GettingStartedHost
             {
                 Console.WriteLine($"An exception occurred: { ce.Message }");
                 Console.ReadLine();
+
                 selfHost.Abort();
             }
         }
